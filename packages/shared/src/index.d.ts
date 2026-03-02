deb2d8d38bb7923c795c6dcab06a53a33b961bac
Nico Grim <nicogrim12@gmail.com>
2026-03-02 09:10:50 +0100

    Bug fix

--- packages/shared/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type { GameManifest } from './manifest.js';
-export type { GameId, SharedGameRegistry } from './registry.js';
-export type { GameEngine, GameStatus, ActionContext, StatusResult, } from './engine.js';
-export type { TicTacToeState, TicTacToeAction, TicTacToePlayer, PlaceMarkAction, Mark, Cell, } from './games/tictactoe.js';
-export type { Connect4State, Connect4Action, Connect4Player, Connect4Cell, DropAction, } from './games/connect4.js';
-export type { AnyGameState, AnyGameAction, ServerToClientEvents, ClientToServerEvents, RoomErrorCode, ActionErrorCode, RematchErrorCode, GameStats, RoomPlayerInfo, OpenRoomInfo, Match, } from './protocol.js';
-//# sourceMappingURL=index.d.ts.map
